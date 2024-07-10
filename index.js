2abda66381b30890b204ddc2b03c84ea24b808d6
colussim <mcolussi@bluewin.ch>
2024-07-10 12:13:38 +0200

    update set InfluxDBMultiSensorAccessory

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ class InfluxDBMultiSensorAccessory {
   async getSensorData() {
     const fluxQuery = `
       from(bucket: "${this.platform.config.bucket}")
-        |> range(start: -1m)
+        |> range(start: 0)
         |> filter(fn: (r) => r["topic"] == "${this.sensorConfig.topic}")
         |> filter(fn: (r) => r["_field"] == "${this.sensorConfig.field}")
         |> last()
